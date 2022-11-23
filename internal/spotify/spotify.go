96c8993844f5fbc4e9c0d3e64cdd70c47aab4cb5
Tim Brammer <tim.brammer@threecomma-ext.io>
2022-11-22 21:14:37 -0600

    finished v1

--- internal/spotify/spotify.go
+++ internal/spotify/spotify.go
@@ -3,6 +3,9 @@ package spotify
 import (
 	"context"
 	"fmt"
+	"sort"
+	"strings"
+	"time"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/timbrammer910/freshly/internal/authenticate"
@@ -34,7 +37,7 @@ func New(cfg *config.Config) *Spotify {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("You are logged in as:", user.ID)
+	log.Infoln("you are logged in as:", user.ID)
 
 	s.client = client
 	s.user = user.ID
@@ -43,21 +46,19 @@ func New(cfg *config.Config) *Spotify {
 }
 
 func (s *Spotify) Freshify(playlists []string, age int, min int) error {
-	playlistIDs, err := s.filterPlaylists(playlists)
+	playlistIDs, err := s.filterPlaylists(playlists, min)
 	if err != nil {
 		return err
 	}
 
-	fmt.Printf("Playlist IDs: %v", playlistIDs)
-
-	// if err := s.processPlaylists(playlistIDs, age, min); err != nil {
-	// 	return err
-	// }
+	if err := s.processPlaylists(playlistIDs, age, min); err != nil {
+		return err
+	}
 
 	return nil
 }
 
-func (s *Spotify) filterPlaylists(playlists []string) ([]spotify.ID, error) {
+func (s *Spotify) filterPlaylists(playlists []string, min int) ([]spotify.ID, error) {
 	var userPlaylists []spotify.ID
 
 	list, err := s.getPlaylists()
@@ -72,11 +73,17 @@ func (s *Spotify) filterPlaylists(playlists []string) ([]spotify.ID, error) {
 		var found bool
 		for _, e := range list {
 			if playlist == e.Name {
+				if int(e.Tracks.Total) <= min {
+					log.Infof("playlist - %s contains too few tracks, skipping...\n", strings.ToLower(e.Name))
+					found = true
+					break
+				}
+
 				if e.Owner.ID == s.user {
 					userPlaylists = append(userPlaylists, e.ID)
 					found = true
+					break
 				}
-				break
 			}
 		}
 		if !found {
@@ -107,4 +114,84 @@ func (s *Spotify) getPlaylists() ([]spotify.SimplePlaylist, error) {
 	}
 
 	return playlists, nil
+}
+
+func (s *Spotify) processPlaylists(playlistIDs []spotify.ID, age int, min int) error {
+	log.Infoln("processing playlists...")
+
+	for _, playlist := range playlistIDs {
+		tracks, err := s.getTracks(playlist)
+		if err != nil {
+			return err
+		}
+
+		err = s.processTracks(tracks, playlist, age, min)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (s *Spotify) getTracks(playlistID spotify.ID) ([]spotify.PlaylistItem, error) {
+	var tracks []spotify.PlaylistItem
+
+	offset := 0
+	limit := 50
+	for {
+		page, err := s.client.GetPlaylistItems(context.Background(), playlistID, spotify.Limit(limit), spotify.Offset(offset))
+		if err != nil {
+			return nil, err
+		}
+
+		tracks = append(tracks, page.Items...)
+		offset = offset + limit
+
+		if page.Next == "" {
+			break
+		}
+	}
+
+	return tracks, nil
+}
+
+func (s *Spotify) processTracks(tracks []spotify.PlaylistItem, playlist spotify.ID, age int, min int) error {
+	sort.Slice(tracks, func(l, g int) bool {
+		lesser, _ := time.Parse(spotify.TimestampLayout, tracks[l].AddedAt)
+		greater, _ := time.Parse(spotify.TimestampLayout, tracks[g].AddedAt)
+
+		return lesser.Unix() < greater.Unix()
+	})
+
+	var deletes []spotify.ID
+	for _, track := range tracks {
+		if tooOld(track, age) {
+			log.Infof("track '%s' is too old\n", strings.ToLower(track.Track.Track.Name))
+			deletes = append(deletes, track.Track.Track.ID)
+			if len(deletes) >= len(tracks)-min {
+				log.Infof("min playlist length reached")
+				break
+			}
+		}
+	}
+
+	_, err := s.client.RemoveTracksFromPlaylist(context.Background(), playlist, deletes...)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func tooOld(track spotify.PlaylistItem, age int) bool {
+	days, _ := time.ParseDuration(fmt.Sprintf("%dh", age*24))
+	added, _ := time.Parse(spotify.TimestampLayout, track.AddedAt)
+	now := time.Now()
+
+	if now.Unix()-added.Unix() > int64(days.Seconds()) {
+		return true
+	}
+
+	return false
 }
